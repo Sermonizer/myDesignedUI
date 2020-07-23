39f88e81135d3ed21f0416afee3aadd204312543
Tengxu <1025753441@qq.com>
2020-07-23 22:01:34 +0800

    menu

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -37,6 +37,7 @@ const generateMenu = (props: MenuProps) => {
   );
 };
 
+// 由于是用css控制submenu出现和消失的，因此添加一段css代码用来测试
 const createStyleFile = () => {
   const cssFile: string = `
   .submenu {
@@ -46,6 +47,7 @@ const createStyleFile = () => {
     display: block
   }
   `;
+  // 创建style文件
   const style = document.createElement("style");
   style.type = "text/css";
   style.innerHTML = cssFile;
@@ -61,6 +63,7 @@ describe("测试Menu和MenuItem组件", () => {
   // 钩子函数 在每个测试运行前都会跑 保存一些可能会重复的
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+    // 插入有CSS样式的HTMLStyleElement节点
     wrapper.container.append(createStyleFile());
     menuElement = wrapper.getByTestId("test-menu");
     activeElement = wrapper.getByText("active");
@@ -73,7 +76,7 @@ describe("测试Menu和MenuItem组件", () => {
     /**
      * 希望测试共有多少个一级li节点，但是getElementsByTagName('li')
      * 获取了所有的li节点，因此与期望的li节点数目不同，会报错
-     * 要判断有几个一级结点，使用css的:scope伪类选择器，选择了menuElement本身的li节点 
+     * 要判断有几个一级结点，使用css的:scope伪类选择器，选择了menuElement本身的li节点
      */
     expect(menuElement.querySelectorAll(":scope > li").length).toEqual(4);
     // expect(menuElement.getElementsByTagName('li').length).toEqual(3)
@@ -94,7 +97,7 @@ describe("测试Menu和MenuItem组件", () => {
     expect(disabledElement).not.toHaveClass("is-active");
     expect(testProps.onSelect).not.toHaveBeenCalledWith("1");
   });
-  
+
   it("should render vertical mode", () => {
     // beforeEach中渲染了wrapper 但是单元开始时又被渲染了一次 导致出错
     // React-testing-library在每个单元结束后都会调用cleanup()方法，将所有渲染回收,因此不同单元之间不会出现问题
@@ -103,23 +106,26 @@ describe("测试Menu和MenuItem组件", () => {
     const menuElement = wrapper.getByTestId("test-menu");
     expect(menuElement).toHaveClass("menu-vertical");
   });
-
+  // 添加subMenu的逻辑
+  // 将回调函数设置为async()类型
   it("should show dropdown items when hover on subMenu", async () => {
+    // 在横向模式时，drop1是隐藏的，不可见
     const dropdownElement = wrapper.getByText("dropdown");
+    // 鼠标移入
     fireEvent.mouseEnter(dropdownElement);
+    // 由于设置了setTimeout，直接使用断言会导致测试失败
+    // 在wait()方法中传入回调函数，添加断言，解决异步问题
     await wait(() => {
-      // 由于是异步执行 因此可以用这种方法，会等待断言执行完毕
       expect(wrapper.queryByText("drop1")).toBeVisible();
     });
+    // 点击第一项，触发onSelect方法
     fireEvent.click(wrapper.getByText("drop1"));
     expect(testProps.onSelect).toHaveBeenCalledWith("3-0");
-
-    fireEvent.mouseLeave(dropdownElement)
+    // 鼠标移出
+    fireEvent.mouseLeave(dropdownElement);
     await wait(() => {
-      // 由于是异步执行 因此可以用这种方法，会等待断言执行完毕
+      // 由于是异步执行，会等待断言执行完毕
       expect(wrapper.queryByText("drop1")).not.toBeVisible();
     });
   });
 });
-
-// expect(wrapper.queryByText("drop1")).not.toBeVisible();
